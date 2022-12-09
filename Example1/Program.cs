bb1dcb99f00f7c14aa93120dfe7225cc55c35860
Antonkina Anastaiya <floweri888@mail.ru>
2022-12-09 09:30:32 +0300

    Add examples5

---
+++ Example1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+bool isPalindrom = true;
+for (int i = 0; i < number.Length/2; i++)
+{
+    if (number[i] != number[number.Length - 1 - i]) {
+        isPalindrom = false;
+        break;
+    }
+}
+
+if (isPalindrom) {
+    Console.WriteLine("палиндром");
+} else {
+    Console.WriteLine("не палиндром");
+}
+
